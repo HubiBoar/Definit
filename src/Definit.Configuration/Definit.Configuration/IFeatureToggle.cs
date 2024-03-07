f9028830e0753e6468055ef3543dc970b6bff0cc
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2024-03-07 21:10:51 +0000

    FeatureToggle as IConfigObject

--- src/Definit.Configuration/Definit.Configuration/IFeatureToggle.cs
+++ src/Definit.Configuration/Definit.Configuration/IFeatureToggle.cs
@@ -1,3 +1,4 @@
+
 namespace Definit.Configuration;
 
 public interface IFeatureName
@@ -5,7 +6,7 @@ public interface IFeatureName
     public static abstract string FeatureName { get; }    
 }
 
-public sealed class FeatureToggle<T> : ISectionName
+public sealed class FeatureToggle<T> : ISectionName, IConfigObject
     where T : IFeatureName
 {
     public delegate Task<bool> Get();
@@ -14,6 +15,7 @@ public sealed class FeatureToggle<T> : ISectionName
 
     public static void Register(IServiceCollection services)
     {
+        services.AddFeatureManagement();
         services.AddSingleton<Get>(provider => () => Create(provider));
     }
 
@@ -29,5 +31,18 @@ public sealed class FeatureToggle<T> : ISectionName
         return await provider
             .GetRequiredService<IFeatureManager>()
             .IsEnabledAsync(T.FeatureName);
+    }
+
+    public static OneOf<Success, ValidationErrors> ValidateConfiguration(IConfiguration configuration)
+    {
+        var featureName = SectionName;
+        var section = configuration.GetSection(featureName);
+
+        if(section is null)
+        {
+            return new ValidationErrors($"Missing FeatureToggle :: [{featureName}]");
+        }
+
+        return new Success();
     }
 }
